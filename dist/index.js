df0bb876ba6fa609fd9bc72fc9f25b143f66ceb8
Vijay <vijaynatarajan@protonmail.com>
2025-01-28 19:02:12 -0500

    added view all roles and departments

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,62 @@
-import { connectToDb } from './connection.js';
+import inquirer from 'inquirer';
+import { pool, connectToDb } from './connection.js';
 let exit = false;
 await connectToDb();
+function main() {
+    inquirer
+        .prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?',
+            choices: [
+                'View All Employees',
+                'Add Employee',
+                'Update Employee Role',
+                'View All Roles',
+                'Add Role',
+                'View All Departments',
+                'Add Department',
+                'Quit'
+            ]
+        }
+    ])
+        .then((answers) => {
+        if (answers.action === 'View All Employees') {
+        }
+        else if (answers.action === 'Add Employee') {
+        }
+        else if (answers.action === 'Update Employee Role') {
+        }
+        else if (answers.action === 'View All Roles') {
+            pool.query('SELECT role.id, role.title, department.name as department, role.salary FROM role JOIN department ON role.department_id = department.id', (err, result) => {
+                if (err) {
+                    console.log(err);
+                }
+                else if (result) {
+                    console.log();
+                    console.table(result.rows);
+                    for (let i = 0; i < result.rows.length; i++) {
+                        console.log();
+                    }
+                }
+            });
+        }
+        else if (answers.action === 'Add Roles') {
+        }
+        else if (answers.action === 'View All Departments') {
+        }
+        else if (answers.action === 'Add Roles') {
+        }
+        else if (answers.action === 'Quit') {
+            exit = true;
+        }
+        if (exit === false) {
+            main();
+        }
+        else {
+            process.exit(0);
+        }
+    });
+}
+main();
